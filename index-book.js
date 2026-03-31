c90939c4e4eeea396cfc6d1e39df52be10156c98
Rajeev Garg <rajeevgarg982@gmail.com>
2026-03-31 23:01:35 +0530

    add change of 31-03-026

--- index-book.js
+++ index-book.js
@@ -21,6 +21,70 @@ const books = [
   }
 ];
 
+
+app.use(express.json());
+
 // Routes
 
-app.get()
+app.get('/books', (req,res) => {
+  res.json(books);
+});
+
+app.get('/books/:id', (req,res) =>{
+
+const id = parseInt(req.params.id);
+if(isNaN(id)){
+  return res.status(400)
+  .json({error: 'id must be a num'});
+
+}
+
+const book = books.find(e=> e.id === id);
+
+if(!book){
+  return res
+  .status(404)
+  .json({error: `book with id ${id} not exist`});
+}
+return res.json(book);
+});
+
+app.post('/books', (req,res) => {
+  
+const { title, author } = req.body;
+if(!title || title === '') return res.status(400).json({error: 'title is required'});
+if(!author || author === '') return res.status(400).json({error: 'author is required'});
+
+const id = books.length + 1 ;
+
+const book = { id , title , author };
+books.push(book);
+return res.status(201).json({message: `books created successfully with id ${id}`});
+
+});
+
+app.delete('/books/:id', (req,res) =>{
+
+const id = parseInt(req.params.id);
+console.log("id",id);
+
+if(isNaN(id)){
+  return res.status(400)
+  .json({error: 'id must be a num'});
+
+}
+
+const bookToDelete = books.findIndex(e=> e.id === id);
+console.log("bookToDelete",bookToDelete);
+
+
+if(bookToDelete < 0 ) {
+  return res
+  .status(404)
+  .json({error: `book with id ${id} not exist`});
+}
+books.splice(bookToDelete,1);
+return res.status(200).json({messgae: "entry deleted"});
+});
+
+app.listen(PORT, () => console.log(`server is running in the PORT ${PORT}`) );
